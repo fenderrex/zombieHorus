45384b8e9f256a69eabb3c442ad9ccb23e1030f2
rex <fenderrex@gmail.com>
2016-09-24 18:16:23 -0700

    opps

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkLobbyManager
+{
+    NetworkLobbyManager n = null;
+    void Start() {
+ 
+       n = new NetworkLobbyManager();
+    }
+    void Update()
+    {
+        //ready=n.CheckReadyToBegin();
+
+    }
+}
