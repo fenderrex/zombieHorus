87fe48b334b9258dda7eb2fae4e1130c20170f8e
rex <rfbaird@sbcc.local>
2016-09-29 17:37:23 -0700

    A tool for anamations
    point to point tranfer

---
+++ Assets/Lobby/nodeFlow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nodeFlow : MonoBehaviour {
+	protected AnimationCurve X;
+	protected AnimationCurve Y;
+	protected AnimationCurve Z;
+	public AnimationCurve TimeShift;
+	public GameObject[] Set;
+	public GameObject prop;
+	// Use this for initialization
+	void Start () {
+		foreach(int i in Set){
+			
+			X.AddKey (i, Set[i].transform.posstion.x);
+			Y.AddKey (i, Set[i].transform.posstion.y);
+			Z.AddKey (i, Set[i].transform.posstion.z);
+		}
+		print ("nodes accounted for!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void FixedUpdate () {
+		float t=Time.time*TimeShift.Evaluate(Time.time);
+		prop.transform.posstion=new Vector(X.Evaluate(t),Y.Evaluate(t),Z.Evaluate(t));
+	}
+
+}
