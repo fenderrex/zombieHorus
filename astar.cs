0d0889be3764a28ba407827531cab4d4065e6738
rex <fenderrex@gmail.com>
2016-10-18 19:27:53 -0700

    "working a star" from stackoverflow

--- astar.cs
+++ astar.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
+using System.Collections.Generic;
+using System.Linq;
 public class astar : MonoBehaviour {
     int[] e = new int[] { };
 	// Use this for initialization
@@ -13,54 +16,138 @@ public class astar : MonoBehaviour {
 	void Update () {
 	
 	}
-}
 
-void AStar(start, goal) {
-// The set of nodes already evaluated.
-GameObject[] closedSet= new GameObject[] { };
-    // The set of currently discovered nodes still to be evaluated.
-    // Initially, only the start node is known.
-GameObject[] openSet = new GameObject[] { start};
-// For each node, which node it can most efficiently be reached from.
-// If a node can be reached from many nodes, cameFrom will eventually contain the
-// most efficient previous step.
-//cameFrom:= the empty map
-GameObject[] cameFrom = new GameObject[] { };
-    // For each node, the cost of getting from the start node to that node.
-    list< GameObject>=new list<GameObject>
-    for (GameObject item in openSet)
+public void unitTest_AStar()
+{
+    char[][] matrix = new char[][] { new char[] {'-', 'S', '-', '-', 'X'},
+                                         new char[] {'-', 'X', 'X', '-', '-'},
+                                         new char[] {'-', '-', '-', 'X', '-'},
+                                         new char[] {'X', '-', 'X', 'E', '-'},
+                                         new char[] {'-', '-', '-', '-', 'X'}};
+
+    //looking for shortest path from 'S' at (0,1) to 'E' at (3,3)
+    //obstacles marked by 'X'
+    int fromX = 0, fromY = 1, toX = 3, toY = 3;
+    matrixNode endNode = AStar(matrix, fromX, fromY, toX, toY);
+
+        //looping through the Parent nodes until we get to the start node
+        Stack<matrixNode> path = new Stack<matrixNode>();
+
+    while (endNode.x != fromX || endNode.y != fromY)
     {
-        item.GetComponent(typeof(astarnode)).fScore();
+        path.Push(endNode);
+        endNode = endNode.parent;
     }
+    path.Push(endNode);
 
-    for (GameObject item in openSet) { 
-        current =//he node in openSet having the lowest fScore[] value
-        if current = goal
-            return reconstruct_path(cameFrom, current)
+    Console.WriteLine("The shortest path from  " +
+                      "(" + fromX + "," + fromY + ")  to " +
+                      "(" + toX + "," + toY + ")  is:  \n");
 
-        openSet.Remove(current)
-        closedSet.Add(current)
-        for each neighbor of current
-            if neighbor in closedSet
-                continue		// Ignore the neighbor which is already evaluated.
-            // The distance from start to a neighbor
-            tentative_gScore:= gScore[current] + dist_between(current, neighbor)
-            if neighbor not in openSet	// Discover a new node
-                openSet.Add(neighbor)
-            else if tentative_gScore >= gScore[neighbor]
-                continue		// This is not a better path.
-   
-    // This path is the best until now. Record it!
-    cameFrom[neighbor] := current
-            gScore[neighbor] := tentative_gScore
-            fScore[neighbor] := gScore[neighbor] + heuristic_cost_estimate(neighbor, goal)
-
-    return failure;
+    while (path.Count > 0)
+    {
+        matrixNode node = path.Pop();
+        Console.WriteLine("(" + node.x + "," + node.y + ")");
     }
 }
-function reconstruct_path(cameFrom, current)
-    total_path := [current]
-    while current in cameFrom.Keys:
-        current := cameFrom[current]
-        total_path.append(current)
-    return total_path
+
+public class matrixNode
+{
+    public int fr = 0, to = 0, sum = 0;
+    public int x, y;
+    public matrixNode parent;
+}
+
+public static matrixNode AStar(char[][] matrix, int fromX, int fromY, int toX, int toY)
+{
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // in this version an element in a matrix can move left/up/right/down in one step, two steps for a diagonal move.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //the keys for greens and reds are x.ToString() + y.ToString() of the matrixNode 
+    Dictionary<string, matrixNode> greens = new Dictionary<string, matrixNode>(); //open 
+    Dictionary<string, matrixNode> reds = new Dictionary<string, matrixNode>(); //closed 
+
+    matrixNode startNode = new matrixNode { x = fromX, y = fromY };
+    string key = startNode.x.ToString() + startNode.x.ToString();
+    greens.Add(key, startNode);
+
+        Func<KeyValuePair<string, matrixNode>> smallestGreen = () =>
+        {
+            bool cat = false;
+      
+            KeyValuePair<string, matrixNode> smallest = new KeyValuePair<string, matrixNode>( greens.Keys.ToArray()[0],greens[greens.Keys.ToArray()[0]] );
+
+
+
+            foreach (KeyValuePair<string, matrixNode> item in greens)
+            {
+               
+                    if (item.Value.sum < smallest.Value.sum)
+                    smallest = item;
+                else if (item.Value.sum == smallest.Value.sum
+                        && item.Value.to < smallest.Value.to)
+                    smallest = item;
+            }
+
+            return smallest;
+        };
+
+        //add these values to current node's x and y values to get the left/up/right/bottom neighbors
+        List<KeyValuePair<int, int>> fourNeighbors = new List<KeyValuePair<int, int>>()
+                                            { new KeyValuePair<int, int>(-1,0),
+                                              new KeyValuePair<int, int>(0,1),
+                                              new KeyValuePair<int, int>(1, 0),
+                                              new KeyValuePair<int, int>(0,-1) };
+
+    int maxX = matrix.GetLength(0);
+    if (maxX == 0)
+        return null;
+    int maxY = matrix[0].Length;
+
+    while (true)
+    {
+        if (greens.Count == 0)
+            return null;
+
+        KeyValuePair<string, matrixNode> current = smallestGreen();
+        if (current.Value.x == toX && current.Value.y == toY)
+            return current.Value;
+
+        greens.Remove(current.Key);
+        reds.Add(current.Key, current.Value);
+
+        foreach (KeyValuePair<int, int> plusXY in fourNeighbors)
+        {
+            int nbrX = current.Value.x + plusXY.Key;
+            int nbrY = current.Value.y + plusXY.Value;
+            string nbrKey = nbrX.ToString() + nbrY.ToString();
+            if (nbrX < 0 || nbrY < 0 || nbrX >= maxX || nbrY >= maxY
+                || matrix[nbrX][nbrY] == 'X' //obstacles marked by 'X'
+                || reds.ContainsKey(nbrKey))
+                continue;
+
+            if (greens.ContainsKey(nbrKey))
+            {
+                matrixNode curNbr = greens[nbrKey];
+                int from = Math.Abs(nbrX - fromX) + Math.Abs(nbrY - fromY);
+                if (from < curNbr.fr)
+                {
+                    curNbr.fr = from;
+                    curNbr.sum = curNbr.fr + curNbr.to;
+                    curNbr.parent = current.Value;
+                }
+            }
+            else
+            {
+                matrixNode curNbr = new matrixNode { x = nbrX, y = nbrY };
+                curNbr.fr = Math.Abs(nbrX - fromX) + Math.Abs(nbrY - fromY);
+                curNbr.to = Math.Abs(nbrX - toX) + Math.Abs(nbrY - toY);
+                curNbr.sum = curNbr.fr + curNbr.to;
+                curNbr.parent = current.Value;
+                greens.Add(nbrKey, curNbr);
+            }
+        }
+    }
+}
+}
