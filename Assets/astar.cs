fad90869cecf1208790f2151cf05df2ed9246bc9
rex <fenderrex@gmail.com>
2016-10-21 14:29:42 -0700

    cube render

--- Assets/astar.cs
+++ Assets/astar.cs
@@ -6,10 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 public class astar : MonoBehaviour {
     int[] e = new int[] { };
-	// Use this for initialization
-	void Start () {
-        
-
+    public bool is3D = true;
+    public GameObject block1;
+    // Use this for initialization
+    void Start () {
+        dataIn datain = new dataIn();
+        //dataIn a=datain.matrix;
+        pathTools.AStar(true, datain.matrix, 1, 1, 1, 3, 3, 3);
+        GameObject block = Instantiate(block1, Vector3.zero, block1.transform.rotation) as GameObject;
+        block.transform.parent = transform;
+        //block.transform.localPosition = new Vector3(x, y, z);
+        //unitTest_AStar();
     }
 	
 	// Update is called once per frame
@@ -19,35 +26,32 @@ public class astar : MonoBehaviour {
 
 public void unitTest_AStar()
 {
-    char[][] matrix = new char[][] { new char[] {'-', 'S', '-', '-', 'X'},
-                                         new char[] {'-', 'X', 'X', '-', '-'},
-                                         new char[] {'-', '-', '-', 'X', '-'},
-                                         new char[] {'X', '-', 'X', 'E', '-'},
-                                         new char[] {'-', '-', '-', '-', 'X'}};
 
-    //looking for shortest path from 'S' at (0,1) to 'E' at (3,3)
-    //obstacles marked by 'X'
-    int fromX = 0, fromY = 1,fromZ=0, toX = 3, toY = 3, toZ=3;
-    matrixNode endNode = AStar(matrix, fromX, fromY, fromZ, toX, toY, toZ);
 
-        //looping through the Parent nodes until we get to the start node
-        Stack<matrixNode> path = new Stack<matrixNode>();
+        //looking for shortest path from 'S' at (0,1) to 'E' at (3,3)
+        //obstacles marked by 'X'
+        dataIn datain = new dataIn();
+        int fromX = 1, fromY = 1,fromZ=1, toX = 3, toY = 3, toZ=3;
+    matrixNode endNode = pathTools.AStar(true, datain.matrix, fromX, fromY, fromZ, toX, toY, toZ);
 
-    while (endNode.x != fromX || endNode.y != fromY)
+    //looping through the Parent nodes until we get to the start node
+    Stack<matrixNode> path = new Stack<matrixNode>();
+    
+    while (endNode.x != fromX || endNode.y != fromY || endNode.z != fromZ)
     {
         path.Push(endNode);
         endNode = endNode.parent;
     }
     path.Push(endNode);
 
-    Console.WriteLine("The shortest path from  " +
-                      "(" + fromX + "," + fromY + ")  to " +
-                      "(" + toX + "," + toY + ")  is:  \n");
+    print("The shortest path from  " +
+                      "(" + fromX + "," + fromY +","+ fromZ+")  to " +
+                      "(" + toX + "," + toY + "," + toZ + ")  is:  \n");
 
     while (path.Count > 0)
     {
         matrixNode node = path.Pop();
-        Console.WriteLine("(" + node.x + "," + node.y + ")");
+            print("(" + node.x + "," + node.y + ","+node.z+")");
     }
 }
 
@@ -57,20 +61,78 @@ public class matrixNode
     public int x, y, z;
     public matrixNode parent;
 }
-
-public static matrixNode AStar(char[][] matrix, int fromX, int fromY, int fromZ, int toX, int toY, int toZ)
+public class repNode
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // in this version an element in a matrix can move left/up/right/down in one step, two steps for a diagonal move.
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //the keys for greens and reds are x.ToString() + y.ToString() of the matrixNode 
-    Dictionary<string, matrixNode> greens = new Dictionary<string, matrixNode>(); //open 
+    public char value = '-';
+    public int length = 0, height = 0, width=0;
+}
+public class dataIn
+{
+        public class repNode
+        {
+            public char value = '-';
+            public int length = 0, height = 0, width = 0;
+        }
+        public delegate repNode matrixDelegate(int x, int y, int z);
+        public repNode matrix(int x, int y, int z)
+    {
+        char[][][] matrix = new char[][][] {
+new char[][]  {
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'} },
+new char[][]  {
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'} },
+new char[][]  {
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', '-', '-', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'} },
+new char[][]  {
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'} },
+new char[][]  {
+    new char[] {'X', 'X', 'X', 'X', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', '-', 'X', '-', 'X'},
+    new char[] {'X', 'X', 'X', 'X', 'X'} },
+new char[][] {
+    new char[] {'-', '-', '-', '-', 'X'},
+    new char[] {'-', 'X', 'X', '-', '-'},
+    new char[] {'-', '-', 'X', '-', '-'},
+    new char[] {'X', '-', 'X', '-', '-'},
+    new char[] {'-', '-', '-', '-', 'X'}} };
+            print(x);
+            print(y);
+            print(z);
+    return new repNode { length = 4, height = 5, width = 4, value = matrix[x][y][z] };
+    }
+}
+public static class pathTools { 
+public static matrixNode AStar(bool is3D, dataIn.matrixDelegate amatrix, int fromX, int fromY, int fromZ, int toX, int toY, int toZ)
+{
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // in this version an element in a matrix can move left/up/right/down in one step, two steps for a diagonal move.
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        dataIn.repNode a = amatrix(0, 0, 0);
+                //the keys for greens and reds are x.ToString() + y.ToString() of the matrixNode 
+        Dictionary<string, matrixNode> greens = new Dictionary<string, matrixNode>(); //open 
     Dictionary<string, matrixNode> reds = new Dictionary<string, matrixNode>(); //closed 
-    public bool is3D = true;
+    
     matrixNode startNode = new matrixNode { x = fromX, y = fromY , z = fromZ};
-    string key = startNode.x.ToString() + startNode.x.ToString();
-    greens.Add(key, startNode);
+    string key = startNode.x.ToString() + startNode.y.ToString() +startNode.z.ToString();
+        greens.Add(key, startNode);
 
         Func<KeyValuePair<string, matrixNode>> smallestGreen = () =>
         {
@@ -89,7 +151,8 @@ public static matrixNode AStar(char[][] matrix, int fromX, int fromY, int fromZ,
                         && item.Value.to < smallest.Value.to)
                     smallest = item;
             }
-
+            print("smallest");
+            
             return smallest;
         };
         //2D block
@@ -100,46 +163,72 @@ public static matrixNode AStar(char[][] matrix, int fromX, int fromY, int fromZ,
                                               new KeyValuePair<int, int>(1, 0),
                                               new KeyValuePair<int, int>(0,-1) };
         //3d block
-        List<List<int>> sixNeighbors =
-        new List<List<int>>{
-            new  List <int> { 0,0,1},
-            new  List <int> { 0,0,-1},
-            new  List <int> { 0,1,0},
-            new  List <int> { 0,-1,0},
-            new  List <int> { 1,0,0},
-            new  List <int> { -1,0,0}
+        //List<List<int>> sixNeighbors =
+        int[][] sixNeighbors = new int[][] {//use one collider and expand it for the ediges...
+            new  int[] { 0,0,1},
+            new  int[] { 0,0,-1},
+            new  int[] { 0,1,0},
+            new  int[] { 0,-1,0},
+            new  int[] { 1,0,0},
+            new  int[] { -1,0,0}
 
 
         };
-    int maxX = matrix.GetLength(0);
+    //repNode a = new amatrix(0, 0, 0);
+    int maxX = a.length;
     if (maxX == 0)
         return null;
-    int maxY = matrix[0].Length;
-
+    int maxY = a.height;
+        if (maxY == 0)
+        return null;
+    int maxZ = a.width;
+    
     while (true)
     {
-        if (greens.Count == 0)
-            return null;
-
+            if (greens.Count == 0)
+            {
+                print("nowhere togo");
+                return null;
+            }
+            //print("{{");
+            //print(greens.Count);
         KeyValuePair<string, matrixNode> current = smallestGreen();
-        if (current.Value.x == toX && current.Value.y == toY && current.Value.z == toZ)
+            print("header");
+            print(current.Value.x);
+            print(current.Value.z);
+            print(current.Value.x);
+            if (current.Value.x == toX && current.Value.y == toY && current.Value.z == toZ)
             return current.Value;
 
         greens.Remove(current.Key);
         reds.Add(current.Key, current.Value);
 
-        foreach (KeyValuePair<int, int> plusXY in fourNeighbors)
+        foreach (int[] mainVector in sixNeighbors)
         {
-            int nbrX = current.Value.x + plusXY.Key;
-            int nbrY = current.Value.y + plusXY.Value;
-            int nbrZ = 0;
-                if (is3D == true)
-                {
+            int nbrX = current.Value.x + mainVector[0];
+            //if (nbrX < 0)
+            //    nbrX = 0;
+            //if (nbrX > maxX)
+            //    nbrX= maxX;
+            int nbrY = current.Value.y + mainVector[1];
+            //if (nbrY < 0)
+            //    nbrY = 0;
+            //if (nbrY > maxY)
+             //   nbrY = maxY;
+            int nbrZ = current.Value.z + mainVector[2];
+            //if (nbrZ < 0)
+            //    nbrZ = 0;
+            //if (nbrZ > maxZ)
+            //    nbrZ = maxZ;
+            if (is3D == true)
+            {
 
-                }
-            string nbrKey = nbrX.ToString() + nbrY.ToString();
-            if (nbrX < 0 || nbrY < 0 || nbrX >= maxX || nbrY >= maxY
-                || matrix[nbrX][nbrY] == 'X' //obstacles marked by 'X'
+            }
+            string nbrKey = nbrX.ToString() + nbrY.ToString()+ nbrZ.ToString();
+            print(nbrKey);
+            dataIn.repNode b =amatrix(nbrX, nbrY, nbrZ);
+            if (nbrX < 0 || nbrY < 0 || nbrZ < 0 || nbrX >= maxX || nbrY >= maxY || nbrZ >= maxZ
+                || b.value == 'X' //obstacles marked by 'X'
                 || reds.ContainsKey(nbrKey))
                 continue;
 
@@ -165,5 +254,6 @@ public static matrixNode AStar(char[][] matrix, int fromX, int fromY, int fromZ,
             }
         }
     }
+}
 }
 }
